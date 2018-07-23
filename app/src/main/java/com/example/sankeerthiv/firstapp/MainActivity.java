1639ba9eaed726c75407a9d8f135e20a0ac1e476
ukinih <ujwalakini04@gmail.com>
2018-07-23 19:03:40 +0530

    new login code

--- app/src/main/java/com/example/sankeerthiv/firstapp/MainActivity.java
+++ app/src/main/java/com/example/sankeerthiv/firstapp/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View v) {
 
                 Intent active = new Intent(MainActivity.this,ActiveIncident.class);
+
                 startActivity(active);
 
             }
--- app/src/main/java/com/example/sankeerthiv/firstapp/Welcome_Login_Activity.java
+++ app/src/main/java/com/example/sankeerthiv/firstapp/Welcome_Login_Activity.java
@@ -1,106 +1,22 @@
 package com.example.sankeerthiv.firstapp;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class Welcome_Login_Activity extends AppCompatActivity {
-
-    RelativeLayout relativeLogin1;
-    public Button login_button;
-    private TextView login_userid;
-    private TextView login_userpassword;
-    private EditText userName, password;
-
-
-    Handler handler = new Handler();
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-
-            relativeLogin1.setVisibility(View.VISIBLE);
-
-
-        }
-    };
-
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome__login_);
-        relativeLogin1 = (RelativeLayout) findViewById(R.id.relativeLogin1);
-
-        handler.postDelayed(runnable, 3000);
-        userName = (EditText) findViewById(R.id.et_user_id);
-        password = (EditText) findViewById(R.id.et_password);
-        login_userid = (TextView) findViewById(R.id.login_userid);
-        login_userpassword = (TextView) findViewById(R.id.login_userpassword);
-        login_button = (Button) findViewById(R.id.login_button);
-        login_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                String userNameText = userName.getText().toString();
-//                String passwordText = password.getText().toString();
-                Log.v("USERNAME", userName.getText().toString());
-
-//                    if(validate(userName.getText().toString(),password.getText().toString())){
-//                        Intent intent = new Intent(Welcome_Login_Activity.this, MainActivity.class);
-//                        startActivity(intent);
-//
-//                        Context context = Welcome_Login_Activity.this;
-//                        String message= "login successful";
-//                        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
-//
-//                    }
-//                    else {
-//
-//                        Context context = Welcome_Login_Activity.this;
-//                        String message= "Invalid Id or password";
-//                        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
-//
-//
-//                    }
-                ApiData apiData = RetrofitClient.getInstance();
-
-                Call<DataModel> dataModelCall = apiData.getData(userName.getText().toString(), password.getText().toString());
-
-                dataModelCall.enqueue(new Callback<DataModel>() {
-                    @Override
-                    public void onResponse(Call<DataModel> call, Response<DataModel> response) {
-                        DataModel dataModel = response.body();
-                        Log.v("RESPONSE", dataModel.isStatus() + "");
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<DataModel> call, Throwable t) {
-                        Log.v("FAIL", t.getMessage());
-                    }
-                });
-
-
-            }
-        });
-
+        webView = (WebView) findViewById(R.id.webview);
+        System.out.print("yo");
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://login.vmware.com");
 
     }
 }
-
-
-
-
-
